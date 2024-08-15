eee4fa4bcb57a96f682a10cc1844564be2a21f45
Marco Friso <marco.friso@gmail.com>
2024-08-15 12:34:06 +0200

    refactor: move elements to separate components

---
+++ app/restaurant/[slug]/components/Title.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Title = () => (
+  <div className="mt-4 border-b pb-6">
+    <h1 className="font-bold text-5xl">Milesstone Grill</h1>
+  </div>
+);
+
+export default Title;

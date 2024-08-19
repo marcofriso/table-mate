3bf5c9cdd96bd13eb6a0eac2044aa0c879d39386
Marco Friso <marco.friso@gmail.com>
2024-08-19 19:54:12 +0200

    feat: fetch data for Menu components

--- app/restaurant/[slug]/components/Menu.tsx
+++ app/restaurant/[slug]/components/Menu.tsx
@@ -1,15 +1,24 @@
 import React from "react";
+import { Item } from "@prisma/client";
 import MenuCard from "./MenuCard";
 
-const Menu = () => (
+const Menu = ({ menu }: { menu: Item[] }) => (
   <main className="bg-white mt-5">
     <div>
       <div className="mt-4 pb-1 mb-1">
-        <h1 className="font-bold text-3xl">Menu</h1>
+        <h1 className="font-bold text-4xl">Menu</h1>
       </div>
-      <div className="flex flex-wrap justify-between">
-        <MenuCard />
-      </div>
+      {menu.length ? (
+        <div className="flex flex-wrap justify-between">
+          {menu.map((item) => (
+            <MenuCard key={item.id} item={item} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-wrap justify-between">
+          <p>This restaurant does not have a menu</p>
+        </div>
+      )}
     </div>
   </main>
 );

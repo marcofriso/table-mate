31c96e26ee6c0c79e12f9b743741e92ccc208ce4
Marco Friso <marco.friso@gmail.com>
2024-08-20 12:02:58 +0200

    refactor: rename file

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import Menu from "../components/Menu";
-import RestaurantNavBar from "../components/RestaurantNavbar";
+import RestaurantNavBar from "../components/RestaurantNavbars";
 
 const prisma = new PrismaClient();
 

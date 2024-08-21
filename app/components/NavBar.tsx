2bb26ae94828ed0fd0a59c156390e36813f62fbc
Marco Friso <marco.friso@gmail.com>
2024-08-21 13:03:35 +0200

    feat: add AuthModal

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import AuthModal from "./AuthModal";
 
 const NavBar = () => (
   <nav className="bg-white p-2 flex justify-between">
@@ -8,10 +9,10 @@ const NavBar = () => (
     </Link>
     <div>
       <div className="flex">
-        <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
-          Sign in
-        </button>
-        <button className="border p-1 px-4 rounded">Sign up</button>
+        <div className="flex">
+          <AuthModal isSignin={true} />
+          <AuthModal isSignin={false} />
+        </div>
       </div>
     </div>
   </nav>

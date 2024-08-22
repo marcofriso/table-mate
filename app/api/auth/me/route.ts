3ce82b96071fdda03ae2fa9d67be5eec3c61f628
Marco Friso <marco.friso@gmail.com>
2024-08-22 12:46:02 +0200

    feat: identify users with JSON Web Token

---
+++ app/api/auth/me/route.ts
@@ -0,0 +1,75 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import * as jose from "jose";
+import jwt from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+
+export async function GET(request: Request) {
+  const bearerToken = request.headers.get("authorization") as string;
+
+  if (!bearerToken) {
+    return NextResponse.json(
+      { errorMessage: "Unauthorized request" },
+      { status: 401 }
+    );
+  }
+
+  const token = bearerToken.split(" ")[1];
+
+  if (!token) {
+    return NextResponse.json(
+      { errorMessage: "Unauthorized request" },
+      { status: 401 }
+    );
+  }
+
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+  try {
+    await jose.jwtVerify(token, secret);
+  } catch (error) {
+    return NextResponse.json(
+      { errorMessage: "Unauthorized request TI" },
+      { status: 401 }
+    );
+  }
+
+  const payload = jwt.decode(token) as { email: string };
+
+  if (!payload?.email) {
+    return NextResponse.json(
+      { errorMessage: "Unauthorized request" },
+      { status: 401 }
+    );
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: payload.email,
+    },
+    select: {
+      id: true,
+      first_name: true,
+      last_name: true,
+      email: true,
+      city: true,
+      phone: true,
+    },
+  });
+
+  if (!user) {
+    return NextResponse.json(
+      { errorMessage: "User not found" },
+      { status: 401 }
+    );
+  }
+
+  return NextResponse.json({
+    id: user.id,
+    firstName: user.first_name,
+    lastName: user.last_name,
+    phone: user.phone,
+    city: user.city,
+  });
+}

cf981370c80f31bc41467f2a505e4e48787eb70b
Marco Friso <marco.friso@gmail.com>
2024-08-27 12:46:13 +0200

    feat: add availability route

---
+++ app/api/restaurant/[slug]/availability/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+type SlugParam = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function GET(request: NextRequest, { params }: SlugParam) {
+  const slug = params.slug;
+  const day = request.nextUrl.searchParams.get("day");
+  const time = request.nextUrl.searchParams.get("time");
+  const partySize = request.nextUrl.searchParams.get("partySize");
+
+  if (!day || !time || !partySize) {
+    return NextResponse.json(
+      { errorMessage: "Invalid data provided" },
+      { status: 400 }
+    );
+  }
+
+  return NextResponse.json({ slug, day, time, partySize }, { status: 200 });
+}
+
+// http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2024-09-09&time=20:00:00.000Z&partySize=4

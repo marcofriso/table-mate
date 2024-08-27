5e75fa962e21967779650f95fc767062268512b0
Marco Friso <marco.friso@gmail.com>
2024-08-27 17:11:08 +0200

    feat: add useAvailabilities hook

---
+++ utils/hooks/useAvailabilities.ts
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function useAvailabilities() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState<
+    { time: string; available: boolean }[] | null
+  >(null);
+
+  const fetchAvailabilities = async ({
+    slug,
+    partySize,
+    day,
+    time,
+  }: {
+    slug: string;
+    partySize: string;
+    day: string;
+    time: string;
+  }) => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_FRONTEND_SERVER}/api/restaurant/${slug}/availability`,
+        {
+          params: {
+            day,
+            time,
+            partySize,
+          },
+        }
+      );
+
+      console.log(response);
+      setLoading(false);
+      setData(response.data);
+    } catch (error: any) {
+      setLoading(false);
+      setError(error.response.data.errorMessage);
+    }
+  };
+
+  return { loading, data, error, fetchAvailabilities };
+}

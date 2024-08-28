363c930c8ec899b5b4ea182f342d8821c856e66f
Marco Friso <marco.friso@gmail.com>
2024-08-28 12:15:33 +0200

    feat(api/reserve): add validation

--- app/api/restaurant/[slug]/reserve/route.ts
+++ app/api/restaurant/[slug]/reserve/route.ts
@@ -1,10 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
 type SlugParam = {
   params: {
     slug: string;
   };
 };
+
+const prisma = new PrismaClient();
 
 export async function GET(request: NextRequest, { params }: SlugParam) {
   const slug = params.slug;
@@ -15,6 +18,35 @@ export async function GET(request: NextRequest, { params }: SlugParam) {
   if (!day || !time || !partySize) {
     return NextResponse.json(
       { errorMessage: "Invalid data provided" },
+      { status: 400 }
+    );
+  }
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      tables: true,
+      open_time: true,
+      close_time: true,
+      id: true,
+    },
+  });
+
+  if (!restaurant) {
+    return NextResponse.json(
+      { errorMessage: "Restaurant not found" },
+      { status: 400 }
+    );
+  }
+
+  if (
+    new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
+    new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
+  ) {
+    return NextResponse.json(
+      { errorMessage: "Restaurant is not open at that time" },
       { status: 400 }
     );
   }

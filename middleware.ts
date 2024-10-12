105fe333d6fcc9b7fbabd0a2a7c84f08f5fef61b
Marco Friso <marco.friso@gmail.com>
2024-10-12 13:30:48 +0200

    feat(middleware): change CORS handling to support multiple origins

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,63 @@
 import { NextRequest, NextResponse } from "next/server";
 import * as jose from "jose";
 
-export async function middleware(request: NextRequest) {
-  const jwtToken = request.cookies.get("jwt")?.value;
+const allowedOrigins = [
+  "http://localhost:3000/",
+  "https://table-mate.vercel.app/",
+  "https://marcofriso.com/",
+  "https://tablemate.marcofriso.com/",
+];
+const corsOptions = {
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+const authPathsToMatch = ["/api/auth/me", "/bookings", "/reserve"];
 
-  if (!jwtToken) {
-    console.error("No JWT token found in cookies");
-    return NextResponse.redirect(new URL("/", request.url));
+export async function middleware(request: NextRequest) {
+  // CORS
+  if (request.nextUrl.pathname.startsWith("/api/:path*")) {
+    const origin = request.headers.get("origin") ?? "";
+    const isAllowedOrigin = allowedOrigins.includes(origin);
+
+    const isPreflight = request.method === "OPTIONS";
+    if (isPreflight) {
+      const preflightHeaders = {
+        ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
+        ...corsOptions,
+      };
+      return NextResponse.json({}, { headers: preflightHeaders });
+    }
+
+    const response = NextResponse.next();
+    if (isAllowedOrigin) {
+      response.headers.set("Access-Control-Allow-Origin", origin);
+    }
+
+    Object.entries(corsOptions).forEach(([key, value]) => {
+      response.headers.set(key, value);
+    });
+
+    return response;
   }
 
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+  // Auth
+  if (
+    authPathsToMatch.some((path) => request.nextUrl.pathname.startsWith(path))
+  ) {
+    const jwtToken = request.cookies.get("jwt")?.value;
 
-  try {
-    await jose.jwtVerify(jwtToken, secret);
-  } catch (error) {
-    console.error("Invalid JWT token", error);
-    return NextResponse.redirect(new URL("/", request.url));
+    if (!jwtToken) {
+      console.error("No JWT token found in cookies");
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+    try {
+      await jose.jwtVerify(jwtToken, secret);
+    } catch (error) {
+      console.error("Invalid JWT token", error);
+      return NextResponse.redirect(new URL("/", request.url));
+    }
   }
 }
-
-export const config = {
-  matcher: ["/api/auth/me", "/bookings", "/reserve/:slug*"],
-};
